8ee0a63fa6230e1d3d4418e3b9afa5e806ccb8e5
Felipe <felipe@pop-os.localdomain>
2021-08-17 03:23:35 -0300

    (feat) restricted area

--- minhasreceitas/minhasreceitas/settings.py
+++ minhasreceitas/minhasreceitas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -139,3 +140,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
